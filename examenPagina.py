708773352ff32815972d249551545c2f3b7a706a
LILE-Z <LILE-Z>
2023-09-10 22:33:06 -0600

    comentarios

--- examenPagina.py
+++ examenPagina.py
@@ -1,22 +1,23 @@
 class TablaPaginas:
-    def __init__(self, proceso_id):
+    def __init__(self, proceso_id): #Constructor
         self.proceso_id = proceso_id
-        self.paginas = []
+        self.paginas = [] #Lista de Paginas por isntacia de proceso contendra numero de pagina / marco de pagina asociado
 
     def agregar_pagina(self, num_pagina, num_marco):
-        self.paginas.append((num_pagina, num_marco))
+        self.paginas.append((num_pagina, num_marco)) 
 
-    def liberar_paginas(self, marcos):
+    def liberar_paginas(self, marcos): #Sacarlas del marco de pagina
         for _, num_marco in self.paginas:
             marcos[num_marco] = 0
-        self.paginas.clear()
+        self.paginas.clear() # Opcional 
 
     def mostrar(self):
         print(f"\nProceso ID: {self.proceso_id}")
         for num_pagina, num_marco in self.paginas:
             print(f"Numero de Pagina: {num_pagina}, Numero de Marco: {num_marco}")
 
-
+#Funciones 
+#Muestra vector y tablas
 def mostrar_estado(marcos, tablas):
     print("\nEstado actual de las Tablas de Páginas:")
     for tabla in tablas:
@@ -30,31 +31,30 @@ def mostrar_estado(marcos, tablas):
 def main():
     TAMANO_PAGINA = 32
     memoria_ram = int(input("Ingrese la cantidad de memoria RAM disponible (en bytes): "))
-    num_marcos = memoria_ram // TAMANO_PAGINA
-    marcos = [0] * num_marcos
-    N = int(input("Ingrese la cantidad de procesos: "))
-    tablas = []
-    cola_procesos = []
-
+    num_marcos = memoria_ram // TAMANO_PAGINA   #Calcular en base al numero de ram numero de marcos posibles
+    marcos = [0] * num_marcos #En base a ello generar listas por cada pagina que luego contendran al proceso 
+    N = int(input("Ingrese la cantidad de procesos: ")) 
+    tablas = [] #TABLAS DE PAGINA 
+    cola_procesos = [] #Para sacar despues 
     for i in range(N):
         proceso_id = input(f"\nIngrese el identificador del proceso {i + 1}: ")
         tamano_proceso = int(input(f"Ingrese el tamaño del proceso {proceso_id} (en bytes): "))
         tabla = TablaPaginas(proceso_id)
-        num_paginas = -(-tamano_proceso // TAMANO_PAGINA)
+        num_paginas = -(-tamano_proceso // TAMANO_PAGINA) #Calcualr numero de paginas
 
-        for j in range(num_paginas):
-            asignado = False
-            for k in range(num_marcos):
-                if marcos[k] == 0:
+        for j in range(num_paginas): #Agregar todas la paginas 
+            asignado = False #Validador si esta asignado o no de no estarlo dicho sacara procesos en orden FIFO
+            for k in range(num_marcos): 
+                if marcos[k] == 0: #Si esta libre entonces ocupalo
                     marcos[k] = 1
-                    tabla.agregar_pagina(j, k)
+                    tabla.agregar_pagina(j, k) #Realiza el cambio sobre la isntacia 
                     asignado = True
                     break
 
             if not asignado:
-                proceso_a_liberar = cola_procesos.pop(0)
+                proceso_a_liberar = cola_procesos.pop(0) #Elimina de memoria el marco 
                 print(f"Liberando proceso {proceso_a_liberar.proceso_id} de la memoria física.")
-                proceso_a_liberar.liberar_paginas(marcos)
+                proceso_a_liberar.liberar_paginas(marcos) #Le quita a las instacias su lugar en el marco 
                 k = 0
                 while marcos[k] != 0:
                     k += 1
